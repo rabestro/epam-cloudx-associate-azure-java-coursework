22fa3c766061b1718b466e68e5eb961e8986c1ca
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-06-14 20:00:32 +0300

    Update URL settings in module-3
    This commit modifies the Petstore service URLs used in the bash script in module-3. The port numbers were removed from the URLs, and one URL was changed to target the correct address for the Petstore product service.

--- module-3/step-3b.bash
+++ module-3/step-3b.bash
@@ -12,9 +12,9 @@ configure_web_app() {
         --settings \
         WEBSITES_PORT=8080 \
         PETSTOREAPP_SERVER_PORT=8080 \
-        PETSTOREPETSERVICE_URL=https://petstorepetservice-eastus.azurewebsites.net:8080 \
-        PETSTOREPRODUCTSERVICE_URL=https://petstoreproductservice-eastus.azurewebsites.net:8080 \
-        PETSTOREORDERSERVICE_URL=https://petstoreorderservice-eastus.azurewebsites.net:8080
+        PETSTOREPETSERVICE_URL=https://petstorepetservice-eastus.azurewebsites.net \
+        PETSTOREPRODUCTSERVICE_URL=https://petstoreproductservice-eastus.azurewebsites.net \
+        PETSTOREORDERSERVICE_URL=https://petstoreorderservice-eastus.azurewebsites.net
 }
 
 configure_web_app $REGION1
@@ -40,4 +40,4 @@ az webapp config appsettings set \
     --settings \
     WEBSITES_PORT=8080 \
     PETSTOREORDERSERVICE_SERVER_PORT=8080 \
-    PETSTOREPRODUCTSERVICE_URL=https://petstoreproductservice:8080
+    PETSTOREPRODUCTSERVICE_URL=https://petstoreproductservice-eastus.azurewebsites.net
