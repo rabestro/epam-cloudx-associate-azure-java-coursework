2a17f1acd2a8db3d96852172ab96d01fa06219ac
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-06-28 23:20:06 +0300

    Refactor service creation in deploy script
    Simplified the procedure of creating web apps in the deploy services script. This involved combining service and plan into a single 'service_plan' variable for better readability. Additionally, the function 'create_web_app' is now called separately with explicit service type parameters, improving code clarity and understanding.

--- module-5/deploy_services.sh
+++ module-5/deploy_services.sh
@@ -1,22 +1,20 @@
 source ./die.sh
 
 create_web_app() {
-    local service="$1-${REGION}"
-    local plan="$2-${REGION}"
-    
+    local service_plan="asp-$1-${REGION}"
+    local service="$2-${REGION}"
+
     echo "Creating Web App for service: $service in region: $REGION"
-    
+
     az webapp create \
         --resource-group $RESOURCES_GROUP \
-        --plan $plan \
+        --plan $service_plan \
         --name $service \
         --deployment-container-image-name $ACR_NAME.azurecr.io/$service:v1 \
         || die "Failed to create Web App for service: $service in region: $REGION"
 }
 
-create_web_app petstoreapp asp-web
-
-for service in petstorepetservice petstoreorderservice petstoreproductservice; do
-    create_web_app $service asp-api
-done
-
+create_web_app web petstoreapp
+create_web_app api petstorepetservice
+create_web_app api petstoreorderservice
+create_web_app api petstoreproductservice
