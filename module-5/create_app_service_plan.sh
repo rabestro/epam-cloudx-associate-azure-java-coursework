c311a5abdec84797d89223e12b4ad0438ace95e2
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-06-28 20:40:39 +0300

    Add script to create App Service plan
    This commit introduces a new script that automates the creation of an App Service plan in a specified region. It relies on the Azure command-line tool to create the plan and handle any failures.

---
+++ module-5/create_app_service_plan.sh
@@ -0,0 +1,14 @@
+source ./die.sh
+
+service_plan="$1-${REGION}"
+
+echo "Creating App Service plan: $service_plan in region: $REGION"
+
+# Create App Service plan
+az appservice plan create \
+    --name $service_plan \
+    --resource-group $RESOURCES_GROUP \
+    --location $REGION \
+    --sku B1 \
+    --is-linux \
+    || die "Failed to create App Service plan: $service_plan in region: $REGION"
