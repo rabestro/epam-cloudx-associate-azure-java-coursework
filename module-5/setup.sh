1d4a537a82c52f938f86e3d21897c1ae572185d8
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-06-28 23:20:22 +0300

    Enable script executions and add App Insights setup
    Uncommented lines in the setup script to enable the execution of `create_resources.sh` and `build_services.sh`. Renamed the environmental variable `RESOURCES_GROUP` from `rg-module5` to `module5_rg` for consistency reasons. A section was added at the end of the script to create an Application Insights resource in Azure, improving monitoring capabilities.

--- module-5/setup.sh
+++ module-5/setup.sh
@@ -1,13 +1,13 @@
 source ./die.sh
 
-export RESOURCES_GROUP=rg-module5
+export RESOURCES_GROUP=module5_rg
 export ACR_NAME=module5acr
 export REGION=eastus
 
-# az login
-# ./create_resources.sh      || die "Failed to create resources"
+az login
+./create_resources.sh      || die "Failed to create resources"
 [ -d "../petstore" ]       || die "Directory ../petstore does not exist"
-#./build_services.sh        || die "Failed to build images"
+./build_services.sh        || die "Failed to build images"
 
 # Create App Service Plans
 ./create_app_service_plan.sh asp-web
@@ -16,3 +16,10 @@ export REGION=eastus
 # Deploy Services
 ./deploy_services.sh
 ./configure_services.sh
+
+# Create an Application Insights resource
+az monitor app-insights component create \
+    --app PetStoreInsight \
+    --location $REGION \
+    --resource-group $RESOURCES_GROUP \
+    --application-type web
